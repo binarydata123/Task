0e3d1fc68f31058862e99bcf71290795e421507a
binarydata123 <binarydata.sale@gmail.com>
2024-03-07 15:33:51 +0530

    add frontent view

---
+++ role/app/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+const Navbar = () => {
+  return (
+    <>
+    </>
+    // <nav>
+       
+    //    <li><Link href="/">Home</Link></li>
+    //    <li><Link href="/contact">Contact</Link></li>
+    //    <li><Link href="/userlogin"> Userlogin</Link></li>
+        
+       
+       
+    // </nav>
+  )
+}
+
+export default Navbar
