1c4bd31dacc88621f6e94c5db418457c76835e12
binarydata123 <binarydata.sale@gmail.com>
2024-03-08 16:59:11 +0530

    add h1 tag

--- role/app/components/navbar/Navbar.tsx
+++ role/app/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
        <li><Link href="/home"> home </Link></li>
        <li><Link href="/about"> about</Link></li>
        <li><Link href="/contact">contact</Link></li>
-        
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam esse cum a vero ex ea accusamus repudiandae, eaque, omnis possimus tempora porro.</h1>
        
        
     </nav>
